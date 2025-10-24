f8519ad742c21054bfcd61f809793b2430103a63
wcson <itnj.wcson@gmail.com>
2025-10-24 17:38:52 +0900

    start

---
+++ src/main/kotlin/inflearn/geminsample/GeminSampleApplication.kt
@@ -0,0 +1,11 @@
+package inflearn.geminsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeminSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeminSampleApplication>(*args)
+}
---
+++ src/test/kotlin/inflearn/geminsample/GeminSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package inflearn.geminsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeminSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
